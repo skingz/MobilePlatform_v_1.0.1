668a1fe0f3a65c23279d1d8e600dccc67843aab6
tjt852 <tjt852@gmail.com>
2013-10-29 11:03:36 +0800

    去掉多余的引用

--- MobilePlat/src/com/leading/mobileplat/ui/WidgetMsg.java
+++ MobilePlat/src/com/leading/mobileplat/ui/WidgetMsg.java
@@ -6,7 +6,6 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ExpandableListView;
