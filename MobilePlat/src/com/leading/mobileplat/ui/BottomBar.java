12b6ee11cec0dacf84f811a4a31136e9942690c8
skingz <skingz@126.com>
2013-10-30 12:21:50 +0800

    Update BottomBar.java
    1

--- MobilePlat/src/com/leading/mobileplat/ui/BottomBar.java
+++ MobilePlat/src/com/leading/mobileplat/ui/BottomBar.java
@@ -34,14 +34,24 @@ public class BottomBar extends RelativeLayout {
 		tv_Setting=(TextView)this.findViewById(R.id.imgBtnSetting);
 		badgeView=new BadgeView(context,tv_Msg);badgeView.setText("New");
 		//badgeView.show();
-		tv_SelectView=(TextView)this.findViewById(R.id.imgSelectedbg);//hideNewMsgSite();
+		//tv_SelectView=(TextView)this.findViewById(R.id.imgSelectedbg);//hideNewMsgSite();
 	}
 	public void setCurrentIcon(TextView tv){
-		//添加滑动效果
+		//初始化
 		if(tv_Current==null){
+			if(tv==tv_Msg){
+				tv_SelectView=(TextView)this.findViewById(R.id.imgSelectedbg);
+			}else if(tv==tv_AppMger){
+				tv_SelectView=(TextView)this.findViewById(R.id.imgSelectedbg_3);
+			}else if(tv==tv_Home){
+				tv_SelectView=(TextView)this.findViewById(R.id.imgSelectedbg_2);
+			}else if(tv==tv_Setting){
+				tv_SelectView=(TextView)this.findViewById(R.id.imgSelectedbg_4);
+			}
+			tv_SelectView.setBackgroundResource(R.drawable.bottom_selected);
 			tv_Current=tv_Msg;
-		}
-		if(tv_Current!=null&&tv!=tv_Current){
+		}//添加滑动效果
+		else if(tv_Current!=null&&tv!=tv_Current){
 			Animation animation=new TranslateAnimation(tv_Current.getLeft(),tv.getLeft(),0,0);
 			animation.setFillAfter(true);
 			animation.setDuration(100);
